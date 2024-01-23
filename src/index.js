fdf724b36574f9cb8625c9534d89b129155ea9f1
Harsh Sharma <harshsharma20503@gmail.com>
2024-01-23 13:41:29 +0530

    added util functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+    .then(() => {
+        console.log("Database connected");
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
